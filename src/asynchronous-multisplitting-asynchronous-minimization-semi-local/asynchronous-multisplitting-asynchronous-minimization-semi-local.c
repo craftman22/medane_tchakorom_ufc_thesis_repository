4a35fd88df83c30aa8c929c2853af82f30bf2d42
craftman <medanetchakorom@gmail.com>
2025-03-11 17:38:33 +0100

    fix: correct the vector given to outer solver for computing the minimized x

--- src/asynchronous-multisplitting-asynchronous-minimization-semi-local/asynchronous-multisplitting-asynchronous-minimization-semi-local.c
+++ src/asynchronous-multisplitting-asynchronous-minimization-semi-local/asynchronous-multisplitting-asynchronous-minimization-semi-local.c
@@ -221,12 +221,12 @@ int main(int argc, char **argv)
         PetscCall(MatMatMult(A_block_jacobi, S, MAT_REUSE_MATRIX, PETSC_DETERMINE, &R));
 
 #ifdef VERSION1
-        PetscCall(outer_solver(comm_jacobi_block, outer_ksp, x_block_jacobi[rank_jacobi_block], R, S, R_transpose_R, vec_R_transpose_b_block_jacobi, alpha, b_block_jacobi[rank_jacobi_block], rank_jacobi_block, s, number_of_iterations));
+        PetscCall(outer_solver(comm_jacobi_block, outer_ksp, x_minimized, R, S, R_transpose_R, vec_R_transpose_b_block_jacobi, alpha, b_block_jacobi[rank_jacobi_block], rank_jacobi_block, s, number_of_iterations));
 #endif
 
 #ifdef VERSION2
         PetscCall(updateLocalRHS(local_right_side_vector, A_block_jacobi_subMat,x_block_jacobi, b_block_jacobi, mat_mult_vec_result, rank_jacobi_block));
-        PetscCall(outer_solver(comm_jacobi_block, outer_ksp, x_block_jacobi[rank_jacobi_block], R, S, R_transpose_R, vec_R_transpose_b_block_jacobi, alpha, local_right_side_vector, rank_jacobi_block, s, number_of_iterations));
+        PetscCall(outer_solver(comm_jacobi_block, outer_ksp, x_minimized, R, S, R_transpose_R, vec_R_transpose_b_block_jacobi, alpha, local_right_side_vector, rank_jacobi_block, s, number_of_iterations));
 #endif
 
         PetscCall(VecWAXPY(approximate_residual, -1.0, x_minimized_prev_iteration, x_minimized));
