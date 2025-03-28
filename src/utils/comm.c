934fa3708454f929fd89b314427e9e0e68a12650
craftman <medanetchakorom@gmail.com>
2025-03-28 14:09:49 +0100

    chore: temporary commit

--- src/utils/comm.c
+++ src/utils/comm.c
@@ -16,9 +16,9 @@ PetscErrorCode comm_async_probe_and_receive(Vec *x_block_jacobi, PetscScalar *rc
         PetscCall(VecGetArray(x_block_jacobi[idx_non_current_block], &rcv_buffer));
         do
         {
-            // printf("=============Block rank %d START multipsplitting RCV communication\n", idx_non_current_block);
+            printf("=============Block rank %d START multipsplitting RCV communication\n", idx_non_current_block);
             PetscCallMPI(MPI_Recv(rcv_buffer, vec_local_size, MPIU_SCALAR, message_source, (TAG_MULTISPLITTING_DATA + idx_non_current_block), MPI_COMM_WORLD, MPI_STATUS_IGNORE));
-            // printf("=============Block rank %d END multipsplitting RCV communication\n", idx_non_current_block);
+            printf("=============Block rank %d END multipsplitting RCV communication\n", idx_non_current_block);
             PetscCallMPI(MPI_Iprobe(message_source, (TAG_MULTISPLITTING_DATA + idx_non_current_block), MPI_COMM_WORLD, &rcv_data_flag, MPI_STATUS_IGNORE));
         } while (rcv_data_flag);
         PetscCall(VecRestoreArray(x_block_jacobi[idx_non_current_block], &rcv_buffer));
