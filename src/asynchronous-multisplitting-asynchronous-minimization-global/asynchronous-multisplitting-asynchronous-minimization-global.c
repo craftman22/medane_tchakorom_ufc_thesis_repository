59241ef4af2257f81d276fb4414e8154f03fc38d
craftman <medanetchakorom@gmail.com>
2025-04-12 20:13:31 +0200

    chore: temporary commit

--- src/asynchronous-multisplitting-asynchronous-minimization-global/asynchronous-multisplitting-asynchronous-minimization-global.c
+++ src/asynchronous-multisplitting-asynchronous-minimization-global/asynchronous-multisplitting-asynchronous-minimization-global.c
@@ -263,7 +263,7 @@ int main(int argc, char **argv)
         PetscCall(MatMatMult(A_block_jacobi_resdistributed, S, MAT_REUSE_MATRIX, PETSC_DETERMINE, &R_block_jacobi_subMat[rank_jacobi_block]));
         PetscCall(restoreHalfSubMatrixToR(R, R_block_jacobi_subMat, rank_jacobi_block));
 
-        PetscCall(comm_async_probe_and_receive_min(R, rcv_minimization_data_buffer, temp_minimization_data_buffer, R_local_values_count, rcv_minimization_data_flag, message_source, rank_jacobi_block, idx_non_current_block, n_mesh_points, rstart, rend, lda, s));
+        // PetscCall(comm_async_probe_and_receive_min(R, rcv_minimization_data_buffer, temp_minimization_data_buffer, R_local_values_count, rcv_minimization_data_flag, message_source, rank_jacobi_block, idx_non_current_block, n_mesh_points, rstart, rend, lda, s));
 
         PetscCall(comm_async_test_and_send_min(R, send_minimization_data_buffer, temp_minimization_data_buffer, send_minimization_data_request, R_local_values_count, message_destination, rank_jacobi_block));
 
