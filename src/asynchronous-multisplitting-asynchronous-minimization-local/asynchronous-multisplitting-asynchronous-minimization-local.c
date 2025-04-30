417d1563c273f5b939fbc8d8782345a578519862
craftman <medanetchakorom@gmail.com>
2025-04-30 14:48:33 +0200

    chore: temporary commit

--- src/asynchronous-multisplitting-asynchronous-minimization-local/asynchronous-multisplitting-asynchronous-minimization-local.c
+++ src/asynchronous-multisplitting-asynchronous-minimization-local/asynchronous-multisplitting-asynchronous-minimization-local.c
@@ -942,19 +942,24 @@ int main(int argc, char **argv)
             n_vectors_inserted++;
         }
 
-        PetscCall(MatAssemblyBegin(S, MAT_FINAL_ASSEMBLY));
-        PetscCall(MatAssemblyEnd(S, MAT_FINAL_ASSEMBLY));
+        if (inner_solver_iterations_count > 1)
+        {
+            PetscCall(MatAssemblyBegin(S, MAT_FINAL_ASSEMBLY));
+            PetscCall(MatAssemblyEnd(S, MAT_FINAL_ASSEMBLY));
 
-        PetscCall(MatMatMult(A_block_jacobi_subMat[rank_jacobi_block], S, MAT_REUSE_MATRIX, PETSC_DETERMINE, &R));
+            PetscCall(MatMatMult(A_block_jacobi_subMat[rank_jacobi_block], S, MAT_REUSE_MATRIX, PETSC_DETERMINE, &R));
 
-        PetscCall(updateLocalRHS(local_right_side_vector, A_block_jacobi_subMat, x_block_jacobi, b_block_jacobi, mat_mult_vec_result, rank_jacobi_block));
-        PetscCall(outer_solver(comm_jacobi_block, outer_ksp, x_block_jacobi[rank_jacobi_block], R, S, R_transpose_R, vec_R_transpose_b_block_jacobi, alpha, local_right_side_vector, rank_jacobi_block, s, number_of_iterations));
+            PetscCall(updateLocalRHS(local_right_side_vector, A_block_jacobi_subMat, x_block_jacobi, b_block_jacobi, mat_mult_vec_result, rank_jacobi_block));
+            PetscCall(outer_solver(comm_jacobi_block, outer_ksp, x_block_jacobi[rank_jacobi_block], R, S, R_transpose_R, vec_R_transpose_b_block_jacobi, alpha, local_right_side_vector, rank_jacobi_block, s, number_of_iterations));
+        }
 
-        PetscCall(VecWAXPY(approximate_residual, -1.0, x_part_minimized_prev_iteration, x_block_jacobi[rank_jacobi_block]));
+        if (inner_solver_iterations_count > 0)
+        {
+            PetscCall(VecWAXPY(approximate_residual, -1.0, x_part_minimized_prev_iteration, x_block_jacobi[rank_jacobi_block]));
+        }
+
         PetscCall(VecNorm(approximate_residual, NORM_INFINITY, &approximation_residual_infinity_norm));
-
-        // PetscCall(VecCopy(x_block_jacobi[rank_jacobi_block], x_part_minimized_prev_iteration));
-
+        
         PetscCall(printResidualNorm(comm_jacobi_block, rank_jacobi_block, approximation_residual_infinity_norm, number_of_iterations));
 
         if (PetscApproximateLTE(approximation_residual_infinity_norm, relative_tolerance) && inner_solver_iterations_count > 0)
