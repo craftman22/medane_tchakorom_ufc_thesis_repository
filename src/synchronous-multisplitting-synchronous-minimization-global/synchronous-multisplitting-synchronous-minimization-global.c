4de2f8cfa4fde1b2d009422bdfd3d5538ecf7ef1
craftman <medanetchakorom@gmail.com>
2025-07-25 14:02:37 +0200

    chore: temporary commit

--- src/synchronous-multisplitting-synchronous-minimization-global/synchronous-multisplitting-synchronous-minimization-global.c
+++ src/synchronous-multisplitting-synchronous-minimization-global/synchronous-multisplitting-synchronous-minimization-global.c
@@ -238,9 +238,9 @@ int main(int argc, char **argv)
     {
       send_signal = CONVERGENCE_SIGNAL;
     }
-    // PetscScalar direct_residual_norm;
-    // PetscCall(computeFinalResidualNorm(A_block_jacobi, x_minimized, b_block_jacobi, rank_jacobi_block, proc_global_rank, &direct_residual_norm));
-    // PetscCall(printFinalResidualNorm(direct_residual_norm));
+    PetscScalar direct_residual_norm;
+    PetscCall(computeFinalResidualNorm(A_block_jacobi, x_minimized, b_block_jacobi, rank_jacobi_block, proc_global_rank, &direct_residual_norm));
+    PetscCall(printFinalResidualNorm(direct_residual_norm));
 
     PetscCall(VecScatterBegin(scatter_jacobi_vec_part_to_merged_vec[idx_non_current_block], x_minimized, x_block_jacobi[idx_non_current_block], INSERT_VALUES, SCATTER_REVERSE));
     PetscCall(VecScatterEnd(scatter_jacobi_vec_part_to_merged_vec[idx_non_current_block], x_minimized, x_block_jacobi[idx_non_current_block], INSERT_VALUES, SCATTER_REVERSE));
