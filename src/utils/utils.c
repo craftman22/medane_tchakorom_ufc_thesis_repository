ccdfbf157d90d1cf665047cf0e21b738157db909
craftman <medanetchakorom@gmail.com>
2025-09-23 13:50:52 +0200

    chore: temporary commit

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1120,7 +1120,7 @@ PetscErrorCode outer_solver_norm_equation_modify_async(MPI_Comm comm_jacobi_bloc
 PetscErrorCode MyConvergeTest(KSP ksp, PetscInt it, PetscReal rnorm, KSPConvergedReason *reason, void *ctx)
 {
   static PetscReal prev_rnorm = -1;
-  if (prev_rnorm > 0 && PetscAbsReal(prev_rnorm - rnorm) / prev_rnorm < 1e-50)
+  if (prev_rnorm > 0 && PetscAbsReal(prev_rnorm - rnorm) / prev_rnorm < 1e-14)
   {
     // PetscPrintf(PETSC_COMM_WORLD, "Possible stagnation at iter %d: residual ratio ~1\n", it);
     *reason = KSP_DIVERGED_BREAKDOWN; // or define a custom code
