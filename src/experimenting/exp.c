efd10a1006a1e98a72c0afcd303c62a02bdf08a5
craftman <medanetchakorom@gmail.com>
2025-09-27 13:51:51 +0200

    chore: temporary commit

--- src/experimenting/exp.c
+++ src/experimenting/exp.c
@@ -12,9 +12,11 @@
 int main(int argc, char **argv)
 {
 
-
     PetscMPIInt nprocs;
     PetscMPIInt proc_global_rank;
+
+    char hostname[PETSC_MAX_PATH_LEN];
+    size_t len;
 
     PetscFunctionBeginUser;
     PetscCall(PetscInitialize(&argc, &argv, NULL, NULL));
@@ -22,11 +24,17 @@ int main(int argc, char **argv)
     PetscCallMPI(MPI_Comm_rank(PETSC_COMM_WORLD, &proc_global_rank));
     PetscCallMPI(MPI_Comm_size(PETSC_COMM_WORLD, &nprocs));
 
-    if (proc_global_rank)
+    PetscCall(PetscGetHostName(hostname, sizeof(hostname)));
+    PetscCall(PetscStrlen(hostname, &len));
+
+    if (proc_global_rank <= 1)
     {
+        if (proc_global_rank == 1)
+            PetscCall(PetscSleep(2));
+
         extern char **environ; // POSIX global for env vars
         char **env = environ;
-        PetscPrintf(PETSC_COMM_SELF, "=== Environment variables visible to rank %d ===\n", proc_global_rank);
+        PetscPrintf(PETSC_COMM_SELF, "=== Environment variables visible to rank %d %s ===\n", proc_global_rank,hostname);
         while (*env)
         {
             PetscPrintf(PETSC_COMM_SELF, "%s\n", *env);
